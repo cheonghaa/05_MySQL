370dca04411e30f6d4bc450a33647a82e0639d3a
DYA <148474216+cheonghaa@users.noreply.github.com>
2024-07-16 17:45:33 +0900

    ScoulaTodo 실습 수정

--- ScoulaTodo/src/main/java/org/scoula/todo/context/Context.java
+++ ScoulaTodo/src/main/java/org/scoula/todo/context/Context.java
@@ -9,7 +9,7 @@ import org.scoula.todo.domain.UserVO;
 public class Context {
     private UserVO user; // 로그인 사용자 정보, null이면 로그아웃 상태
     private Context() {}
-    private static Context context = new Context();
+    private static Context context = new Context(); //싱글톤
     public static Context getContext() {
         return context;
     }

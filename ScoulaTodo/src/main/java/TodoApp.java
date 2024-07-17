ffaeedfeb7d86c45c06bfbf970cb8dafc6da147a
DYA <148474216+cheonghaa@users.noreply.github.com>
2024-07-17 11:49:29 +0900

    24.07.17 ScoulaTodo 실습

--- ScoulaTodo/src/main/java/TodoApp.java
+++ ScoulaTodo/src/main/java/TodoApp.java
@@ -6,6 +6,7 @@ import org.scoula.todo.context.Context;
 import org.scoula.todo.exception.LoginFailException;
 import org.scoula.todo.service.AccountService;
 import org.scoula.todo.service.LoginService;
+import org.scoula.todo.service.TodoService;
 
 import java.sql.SQLException;
 
@@ -15,7 +16,12 @@ public class TodoApp extends App {
 
     LoginService loginService = new LoginService();
     AccountService accountService = new AccountService();
+    TodoService todoService = new TodoService(); // 예시: TodoService 클래스의 인스턴스 생성
 
+    public static void main(final String[] args) {
+        final TodoApp app = new TodoApp();
+        app.run();
+    }
 
     @Override
     public void init() {
@@ -25,13 +31,22 @@ public class TodoApp extends App {
         anonymousMenu.add(new MenuItem("종료", this::exit));
 
         userMenu = new Menu();
+        userMenu.add(new MenuItem("목록", todoService::print));
+        userMenu.add(new MenuItem("검색", todoService::search));
+        userMenu.add(new MenuItem("상세", todoService::detail));
+        userMenu.add(new MenuItem("추가", todoService::create));
+        userMenu.add(new MenuItem("수정", todoService::update));
+        userMenu.add(new MenuItem("삭제", todoService::delete));
+
         userMenu.add(new MenuItem("로그아웃", this::logout));
         userMenu.add(new MenuItem("종료", this::exit));
 
         setMenu(anonymousMenu); // 시작은 annonymousMenu로
     }
-    public void join(){
+
+    public void join() {
     }
+
     public void login() {
         try {
             loginService.login();
@@ -42,22 +57,17 @@ public class TodoApp extends App {
             System.out.println(e.getMessage());
         }
     }
+
     public void logout() {
-        if(Input.confirm("로그아웃 할까요?")) {
+        if (Input.confirm("로그아웃 할까요?")) {
             Context.getContext().setUser(null);
             setMenu(anonymousMenu); // 메뉴 교체
         }
     }
 
-    public void exit(){
+    public void exit() {
 
         System.exit(0);
-    }
-
-
-    public static void main(final String[] args) {
-        final TodoApp app = new TodoApp();
-        app.run();
     }
 
 
---
+++ ScoulaTodo/src/main/java/org/scoula/todo/dao/TodoDaoImpl.java
@@ -0,0 +1,125 @@
+package org.scoula.todo.dao;
+
+import org.scoula.todo.common.JDBCUtil;
+import org.scoula.todo.domain.TodoVO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TodoDaoImpl implements TodoDao{
+    Connection conn = JDBCUtil.getConnection();
+    @Override
+    public int getTotalCount(String userId) throws SQLException {
+        String sql = "select count(*) from todo where userid=?";        //집계함수 사용하기
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, userId);
+            try(ResultSet rs = stmt.executeQuery()) {                   //select query는 executeQuery로 호출
+                rs.next();
+                return rs.getInt(1);
+            }
+        }
+    } @Override
+    public int create(TodoVO todo) throws SQLException {
+        String sql = "insert into todo(title, description, done, userid) values(?,?,?,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, todo.getTitle());
+            stmt.setString(2, todo.getDescription());
+            stmt.setBoolean(3, todo.getDone());
+            stmt.setString(4, todo.getUserId());
+            return stmt.executeUpdate();
+        }
+    }
+    private TodoVO map(ResultSet rs) throws SQLException {
+        TodoVO todo = new TodoVO();
+        todo.setId(rs.getLong("id"));
+        todo.setTitle(rs.getString("title"));
+        todo.setDescription(rs.getString("description"));
+        todo.setDone(rs.getBoolean("done"));
+        todo.setUserId(rs.getString("userid"));
+        return todo;
+    }
+
+    private List<TodoVO> mapList(ResultSet rs) throws SQLException { //getList, search에서 사용
+        List<TodoVO> todoList = new ArrayList<TodoVO>(); //비어있는 ArrayList 준비
+        while (rs.next()) {
+            TodoVO todo = map(rs); //행 하나하나 맵으로 변환 시키고
+            todoList.add(todo);    // 리스트에 넣어준다.
+        }
+        return todoList;
+    }
+
+    @Override
+    public List<TodoVO> getList(String user) throws SQLException
+    {
+        String sql = "select * from todo where userId = ?";         //로그인한 사용자 ID를 Where절에 작성
+        try (PreparedStatement stmt = conn.prepareStatement
+                (sql)){
+            stmt.setString(1, user);
+            try(ResultSet rs = stmt.executeQuery()) {
+                return mapList(rs);                                 //앞에서 만들었던 mapList 호출 후 사용
+            }
+        }
+    }
+
+    @Override
+    public Optional<TodoVO> get(String userId, Long id) throws SQLException
+    {
+        String sql = "select * from todo where userId = ? and id = ?";
+        try (PreparedStatement stmt = conn.prepareStatement
+                (sql)) {
+            stmt.setString(1, userId);
+            stmt.setLong(2, id);
+            try(ResultSet rs = stmt.executeQuery()) {
+                if(rs.next()) {
+                    return Optional.of(map(rs));
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    //Search : 검색어가 전달 됨
+    @Override
+    public List<TodoVO> search(String userId, String keyword) throws SQLException {
+        String sql = "select * from todo where userId = ? and (title like ? or description like ?)";
+        // -> 어디에서 검색을 할 것인가 (제목과 description에)
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, userId);
+            stmt.setString(2, keyword);
+            stmt.setString(3, keyword);
+            try(ResultSet rs = stmt.executeQuery()) {
+                return mapList(rs);
+            }
+        }
+    }
+
+    @Override
+    public int update(String userId, TodoVO todo) throws SQLException {
+        String sql = "update todo set title = ?, description = ?, done = ? where userId =? and id = ?";
+        try ( PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, todo.getTitle());
+            stmt.setString(2, todo.getDescription());
+            stmt.setBoolean(3, todo.getDone());
+            stmt.setString(4, userId);
+            stmt.setLong(5, todo.getId());
+            return stmt.executeUpdate();
+        }
+        //return 0;
+    }
+
+    @Override
+    public int delete(String userId, Long id) throws SQLException {
+        String sql = "delete from todo where userId = ? and id = ?";
+        try(PreparedStatement stmt = conn.prepareStatement(sql)) {              //자동 닫기를 위해 try문 사용
+            stmt.setString(1, userId);
+            stmt.setLong(2, id);
+            return stmt.executeUpdate();
+        }
+        //return 0;
+    }
+}
